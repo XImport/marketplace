6374fe0b95e5031ac739e891aecd11ffa5d6a8d4
XImport <zed.robot99@gmail.com>
2023-04-30 05:40:09 +0100

    alittle bit of structring

---
+++ src/DATA/index.js
@@ -0,0 +1,61 @@
+const DATA = [
+  {
+    id: 1,
+    img: "https://yozimarket.com/1342-home_default/anneaux-de-calamar-500g-islamar-.jpg",
+    Categorie: "Categories 1",
+    description: "ANNEAUX DE CALAMAR 500g ISLAMAR",
+    price: "45,00 MAD",
+  },
+  {
+    id: 2,
+    Categorie: "Categories 2",
+    img: "https://yozimarket.com/1340-home_default/feta-cheese-rolls-12-480g.jpg",
+    description: "FETA CHEESE ROLLS *12 480g",
+    price: "60,00 MAD",
+  },
+
+  {
+    id: 3,
+    Categorie: "Categories 3",
+    img: "https://yozimarket.com/1337-home_default/street-fries-cheddar-crispy-onions-300g-mccain.jpg",
+    description: "STREET FRIES CHEDDAR CRISPY ONIONS...",
+    price: "45,00 MAD",
+  },
+  {
+    id: 4,
+    Categorie: "Categories 2",
+    img: "https://yozimarket.com/1336-home_default/potato-up-aux-herbes-2x90g-mccain.jpg",
+    description: "POTATO'UP AUX HERBES 2X90g MCCAIN",
+    price: "25,00 MAD",
+  },
+  {
+    id: 5,
+    Categorie: "Categories 4",
+    img: "https://yozimarket.com/1335-home_default/frit-up-mccain-180-g-2-90-g.jpg",
+    description: " Frit up - McCain - 180 g (2 * 90 g)",
+    price: "25,00 MAD",
+  },
+  {
+    id: 6,
+    Categorie: "Categories 1",
+    img: "https://yozimarket.com/1334-home_default/crispy-onion-rings-350g-mccain.jpg",
+    description: "CRISPY ONION RINGS 350g MCCAIN",
+    price: "28,00 MAD",
+  },
+  {
+    id: 7,
+    Categorie: "Categories 2",
+    img: "https://yozimarket.com/1333-home_default/nachos-triangles-230g-pickers-mccain-.jpg",
+    description: " NACHOS TRIANGLES 230g PICKERS MCCAIN",
+    price: "35,00 MAD",
+  },
+  {
+    id: 8,
+    Categorie: "Categories 4",
+    img: "https://yozimarket.com/1338-home_default/spiral-pie-cheese-800g-lezza-.jpg",
+    description: "SPIRAL PIE CHEESE 800g LEZZA",
+    price: "56,00 MAD",
+  },
+];
+
+export default DATA;
